31fd7167cf76b9a3cad782b39a0573c2e01b7ac0
Matheus  Felipe Antonio <matheusantoniof@gmail.com>
2022-01-10 21:51:50 -0300

    [FEAT] Enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3333);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
